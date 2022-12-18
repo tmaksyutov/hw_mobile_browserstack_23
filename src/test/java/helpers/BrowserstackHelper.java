2d5ae486b7e47278e3493fb9fe5d474bd0c46222
TMaksyutov <dadojkeee@mail.ru>
2022-12-18 21:40:23 +0300

    edit helpers

--- src/test/java/helpers/BrowserstackHelper.java
+++ src/test/java/helpers/BrowserstackHelper.java
@@ -1,10 +1,15 @@
 package helpers;
 
+import config.BrowserstackConfig;
+import org.aeonbits.owner.ConfigFactory;
+
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
 
 public class BrowserstackHelper {
     //curl -u "dadojkeee_PK7yEs:C8bxrxsfqpDFgawgBWQd" -X GET "https://api.browserstack.com/app-automate/sessions/c3915bd7d824b3cbbf3b72a12ac4cee1e4d8f0b1.json"
+
+    static BrowserstackConfig config = ConfigFactory.create(BrowserstackConfig.class);
     public static String getBrowserstackVideoUrl(String sessionId){
 
         String url = format("https://api.browserstack.com/app-au" +
@@ -12,7 +17,7 @@ public class BrowserstackHelper {
 
         return given()
                 .log().all()
-                .auth().basic("dadojkeee_PK7yEs", "C8bxrxsfqpDFgawgBWQd")
+                .auth().basic(config.user(), config.key())
                 .when()
                 .get(url)
                 .then()
