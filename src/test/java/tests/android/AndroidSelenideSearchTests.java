aa3f3195a6a64428dd3d7c6edd3724e5d37f707f
TMaksyutov <dadojkeee@mail.ru>
2022-12-24 12:14:50 +0300

    added tests

---
+++ src/test/java/tests/android/AndroidSelenideSearchTests.java
@@ -0,0 +1,44 @@
+package tests.android;
+
+import com.codeborne.selenide.CollectionCondition;
+import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Test;
+import tests.TestBase;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class AndroidSelenideSearchTests extends TestBase {
+
+    @Test
+    void successSearchWikiTest() {
+        back();
+        step("Type search", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("JavaScript");
+        });
+        step("Verify content found", () -> {
+            $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(sizeGreaterThan(0));
+        });
+
+    }
+    @Test
+    void countryPageSearchTest() {
+        back();
+        step("Type search", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Russia");
+        });
+        step("Verify content found", () -> {
+            $$(AppiumBy.className("android.widget.TextView"))
+                    .shouldHave(CollectionCondition.sizeGreaterThan(0));
+        });
+        step("Verify page is correct", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
+            $(AppiumBy.className("android.view.View")).click();
+            $(AppiumBy.className("android.widget.TextView")).shouldHave(text("Russia"));
+        });
+    }
+}
