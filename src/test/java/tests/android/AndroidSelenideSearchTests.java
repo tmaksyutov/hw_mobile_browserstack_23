c206b8bb02584fa2e821bf3984a0de88547c8190
TMaksyutov <dadojkeee@mail.ru>
2023-01-10 18:40:35 +0300

    added test

--- src/test/java/tests/android/AndroidSelenideSearchTests.java
+++ src/test/java/tests/android/AndroidSelenideSearchTests.java
@@ -42,4 +42,37 @@ public class AndroidSelenideSearchTests extends TestBase {
             $(AppiumBy.className("android.widget.TextView")).shouldHave(text("Russia"));
         });
     }
+
+    @Test
+    void onBoardingStepsTest() {
+
+        step("Check that the text 'The Free Encyclopedia …in over 300 languages' is visible", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("The Free Encyclopedia …in over 300 languages"));
+        });
+
+        step("Click on 'Сontinue'", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+
+        step("Check that the text 'New ways to explore' is visible", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("New ways to explore"));
+        });
+
+        step("Click on 'Сontinue'", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+
+        step("Check that the text 'Reading lists with sync' is visible", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("Reading lists with sync"));
+        });
+
+        step("Click on 'Сontinue'", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+
+        step("Check that the text 'Send anonymous data' is visible", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("Send anonymous data"));
+        });
+    }
 }
