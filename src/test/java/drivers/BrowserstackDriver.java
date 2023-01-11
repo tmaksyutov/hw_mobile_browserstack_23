f7d3839a656267d2987a5c23a74280b681f41a2f
TMaksyutov <dadojkeee@mail.ru>
2023-01-11 16:18:58 +0300

    fix

--- src/test/java/drivers/BrowserstackDriver.java
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -14,6 +14,7 @@ import java.net.URL;
 
 
 public class BrowserstackDriver implements WebDriverProvider {
+    static BrowserstackConfig config = ConfigFactory.create(BrowserstackConfig.class, System.getProperties());
 
     public static URL getBrowserstackUrl() {
         try {
@@ -23,7 +24,6 @@ public class BrowserstackDriver implements WebDriverProvider {
         }
     }
 
-    static BrowserstackConfig config = ConfigFactory.create(BrowserstackConfig.class, System.getProperties());
 
     @SneakyThrows
     @Override
